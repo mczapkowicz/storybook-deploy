2cc6296386dcd89d23ab057b7d3f8bda32a86af7
mczapkowicz <78358043+mczapkowicz@users.noreply.github.com>
2023-05-12 18:36:04 +0000

    Deploying to gh-pages from @ mczapkowicz/storybook-deploy@e52da353d8170382d58bb67601d833faa91f999a 🚀

---
+++ assets/preview-d5d9d9e7.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-04343e60.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-3b5e0fe9.js"),["./DocsRenderer-EYKKDMVH-3b5e0fe9.js","./iframe-04343e60.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-5792738b.js","./index-9900c05c.js","./index-5b974414.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-d5d9d9e7.js.map
